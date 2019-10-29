e1f0eb15ef68754436488ad41488df2ce7c3af7b
gyetol <econostein@naver.com>
2019-10-29 22:14:33 +0900

    lectopia C HW1

---
+++ C_Language/C_Language/HW1.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#pragma warning (disable:4996)
+
+void printAge(int);
+void printHeight(double);
+
+int main(void)
+{
+	char name[20] = "홍길동";
+	int age = 500;
+	double height = 170.8;
+
+	printf("성명:%s\n", name);
+	printAge(age);
+	printHeight(height);
+
+	return 0;
+}
+
+void printAge(int age)
+{
+	printf("나이:%d\n", age);
+	
+	return;
+}
+void printHeight(double height)
+{
+	printf("신장:%.2lf\n", height);
+
+	return;
+}
