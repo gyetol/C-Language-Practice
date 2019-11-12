0ca50e8c46f28727d3542cde66c4a019551572ac
gyetol <econostein@naver.com>
2019-11-12 22:35:52 +0900

    lectopia C HW15 version 2.0

--- C_Language/C_Language/HW15.cpp
+++ C_Language/C_Language/HW15.cpp
@@ -23,28 +23,46 @@ int main(void)
 void input(double* weight, double* height)
 {
 	int res;
+	char garbage;
+
 	while (1)
 	{
 		printf("몸무게를 입력하세요(kg):");
-		res = scanf("%lf", weight);
-		if (res == 1) { break; }
-		else {
+		res=scanf("%lf%c", weight, &garbage);
+		if(res==2){
+			if (garbage == '\n') { break; }
+			else { 
+				printf("잘못입력하셨습니다.\n");
+				myflush();
+			}
+		}
+		else { 
+			printf("잘못입력하셨습니다.\n");
 			myflush();
 			continue;
 		}
 	}
 	while (1)
 	{
-		printf("키를 입력하세요(m):");
-		res = scanf("%lf", height);
-		if (res == 1) { break; }
+		printf("키를 입력하세요(cm):");
+		res = scanf("%lf%c", height, &garbage);
+		if (res == 2) {
+			if (garbage == '\n') { break; } 
+			else { 
+				printf("잘못입력하셨습니다.\n");
+				myflush();
+			}
+		}
 		else {
+			printf("잘못입력하셨습니다.\n");
 			myflush();
 			continue;
 		}
 	}
+
 	return;
 }
+
 
 double cal(double* weight, double* height)
 {
@@ -78,6 +96,8 @@ void myflush()
 /*
 몸무게나 키 입력시 45.fas처럼 '숫자.문자' 형식으로 입력했을 때 걸러주질 못한다.
 이 부분을 개선해야한다...
+
+-> scanf로 받을때 %c를 한번 더 받아서, %c에 들어오는 놈이 개행이면 통과해주면 된다!!!!
 
 
 */
